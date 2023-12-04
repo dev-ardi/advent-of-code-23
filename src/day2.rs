fb04d2786f449dc280b04ec24863a31951e53139
ardi <oriongonza@gmail.com>
2023-12-04 01:17:59 +0100

    day 3

--- src/day2.rs
+++ src/day2.rs
@@ -1,6 +1,4 @@
-use std::ops::Deref;
-
-use aoc_runner_derive::{aoc, aoc_generator};
+use aoc_runner_derive::aoc;
 
 enum Balls {
     Red(u64),
@@ -14,11 +12,7 @@ fn part1(input: &str) -> u64 {
         l.split(';').map(|input| {
             input.split(", ").map(|str| {
                 let mut split = str.trim().split(' ');
-                let num = split
-                    .next()
-                    .unwrap_or_else(|| panic!("str: {str}"))
-                    .parse::<u64>()
-                    .unwrap_or_else(|e| panic!("error parsing: {e}\n str: {str}"));
+                let num = split.next().unwrap().parse::<u64>().unwrap();
                 match split.next().unwrap() {
                     "red" => Balls::Red(num),
                     "green" => Balls::Green(num),
@@ -63,11 +57,7 @@ fn part2(input: &str) -> u64 {
         l.split(';').map(|input| {
             input.split(", ").map(|str| {
                 let mut split = str.trim().split(' ');
-                let num = split
-                    .next()
-                    .unwrap_or_else(|| panic!("str: {str}"))
-                    .parse::<u64>()
-                    .unwrap_or_else(|e| panic!("error parsing: {e}\n str: {str}"));
+                let num = split.next().unwrap().parse::<u64>().unwrap();
                 match split.next().unwrap() {
                     "red" => Balls::Red(num),
                     "green" => Balls::Green(num),
@@ -121,5 +111,13 @@ mod tests {
     }
 
     #[test]
-    fn part2_example() {}
+    fn part2_example() {
+        let input = "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        let output = part2(&input);
+        assert_eq!(output, 2286);
+    }
 }
