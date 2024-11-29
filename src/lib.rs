04be42f803ff95f75fd27d8a2fa3e967858c1f8d
Orion Gonzalez <ardi@ardis.dev>
2024-11-29 14:53:15 +0100

    remove rls

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,11 @@
-mod day25;
-mod day24;
-mod day23;
-mod day22;
-mod day21;
-mod day20;
-mod day19;
-mod day18;
-mod day17;
-mod day16;
-mod day15;
-mod day14;
-mod day13;
-mod day12;
-mod day11;
-mod day10;
-mod day9;
 mod day8;
-mod day7;
 mod day1;
 mod day2;
 mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 use aoc_runner_derive::aoc_lib;
 
 aoc_lib! { year = 2023 }
